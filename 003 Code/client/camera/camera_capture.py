99ecb5533c49c2f7b09a39a96679504fef60b006
HyoJeong <20181589@edu.hanbat.ac.kr>
2023-12-04 19:08:39 +0900

    init_client_reupload

--- 003 Code/client/camera/camera_capture.py
+++ 003 Code/client/camera/camera_capture.py
@@ -10,7 +10,11 @@ def capture_image():
     current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     file_name = f"image_{current_time}.jpg"
 
+<<<<<<< HEAD
     os.system(f"libcamera-still -t 1 -o {save_directory}/{file_name}")
+=======
+    os.system(f"libcamera-still -t 1 -o {save_directory}/{file_name} --vflip") # 카메라가 거꾸로 설치되어 있어 vflip 사용. 정방향일 경우 해당 부분 삭제
+>>>>>>> 67538fb (init_client_reupload)
     
     return(file_name)
     
